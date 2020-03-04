20ccb7aa01b73145265a716a3be9fa13ad46def9
JULIEN BENAC <julien.benac@iut.local>
2020-03-04 17:40:45 +0100

    fix #1.2 Parser DOM

--- part1-roombooking-xml/src/main/java/xmlws/roombooking/xmltools/RoomBookingDomParser.java
+++ part1-roombooking-xml/src/main/java/xmlws/roombooking/xmltools/RoomBookingDomParser.java
@@ -1,12 +1,33 @@
 package xmlws.roombooking.xmltools;
 
+import org.w3c.dom.Document;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.InputStream;
+import java.text.SimpleDateFormat;
 
 public class RoomBookingDomParser implements RoomBookingParser {
 
+    private RoomBooking rb = new RoomBooking();
+
+    private SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+
     @Override
     public RoomBooking parse(InputStream inputStream) {
-        return null;
+        try {
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document doc = db.parse(inputStream);
+            rb.setRoomLabel(doc.getElementsByTagName("label").item(0).getTextContent());
+            rb.setUsername(doc.getElementsByTagName("username").item(0).getTextContent());
+            rb.setStartDate(sdf.parse(doc.getElementsByTagName("startDate").item(0).getTextContent()));
+            rb.setEndDate(sdf.parse(doc.getElementsByTagName("endDate").item(0).getTextContent()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return rb;
     }
 
 }
